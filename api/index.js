f317a37ce6e5c99347f84223b102f8ee2333b4c9
mejiapj <mejiapj@gmail.com>
2023-05-20 05:29:08 -0500

    Optimizando temperamentoController

--- api/index.js
+++ api/index.js
@@ -20,11 +20,15 @@
 require('dotenv').config();
 
 const server = require('./src/app.js');
+
+const { fetchData } = require('./src/controllers/temperamentController.js');
+
 const { conn } = require('./src/db.js');
 const { PORT } = process.env;
 
 // Syncing all the models at once.
 conn.sync({ force: true }).then(() => {
+  fetchData();
   server.listen(PORT, () => {
     console.log(`%s listening at ${PORT}`); // eslint-disable-line no-console
   });
--- api/src/controllers/temperamentController copy.js
+++ api/src/controllers/temperamentController copy.js
@@ -1,9 +1,10 @@
 const { Temperament } = require('../db');
+
 const axios = require('axios');
 
 async function fetchData() {
   try {
-    const config = {
+    let config = {
       method: 'get',
       maxBodyLength: Infinity,
       url: 'https://api.thedogapi.com/v1/breeds',
@@ -13,9 +14,30 @@ async function fetchData() {
     const response = await axios.request(config);
 
     if (response.data) {
-      const temperaments = extractTemperaments(response.data);
+      // console.log(response.data);
+
+      const temperaments = response.data.reduce((acc, obj) => {
+        if (
+          obj.temperament &&
+          typeof obj.temperament === 'string' &&
+          obj.temperament.trim() !== ''
+        ) {
+          const splitTemperaments = obj.temperament.split(', ');
+          splitTemperaments.forEach((temp) => {
+            const trimmedTemp = temp.trim();
+            const existingTemp = acc.find((item) => item.name === trimmedTemp);
+            if (!existingTemp) {
+              acc.push({ name: trimmedTemp });
+            }
+          });
+        }
+        return acc;
+      }, []);
+
       temperaments.sort((a, b) => a.name.localeCompare(b.name));
-      // console.log(temperaments);
+
+      console.log(temperaments);
+
       await fillDbTemperaments(temperaments);
     }
   } catch (error) {
@@ -23,51 +45,34 @@ async function fetchData() {
   }
 }
 
-function extractTemperaments(data) {
-  const temperaments = data.reduce((acc, obj) => {
-    if (
-      obj.temperament &&
-      typeof obj.temperament === 'string' &&
-      obj.temperament.trim() !== ''
-    ) {
-      const splitTemperaments = obj.temperament.split(', ');
-      splitTemperaments.forEach((temp) => {
-        const trimmedTemp = temp.trim();
-        if (!acc.some((item) => item.name === trimmedTemp)) {
-          acc.push({ name: trimmedTemp });
-        }
-      });
-    }
-    return acc;
-  }, []);
-
-  return temperaments;
-}
-
-async function fillDbTemperaments(temperaments) {
-  // console.log(temperaments);
+const fillDbTemperaments = async (temperaments) => {
+  console.log(temperaments);
   try {
-    if (temperaments.length > 0) {
+    if (temperaments && temperaments.length > 0) {
       await Temperament.bulkCreate(temperaments);
     }
   } catch (error) {
     console.log(error);
   }
-}
+};
 
 const createTemperament = async (req, res, next) => {
-  const { name } = req.body;
+  let { name } = req.body;
+
+  const existingTemperament = await Temperament.findOne({
+    where: { name: name },
+  });
+
+  if (existingTemperament) {
+    return res
+      .status(500)
+      .json({ message: 'El Temperament ya existe en la base de datos' });
+  }
 
   try {
-    const existingTemperament = await Temperament.findOne({ where: { name } });
-
-    if (existingTemperament) {
-      return res
-        .status(500)
-        .json({ message: 'El Temperament ya existe en la base de datos' });
-    }
-
-    await Temperament.create({ name });
+    const TemperamentCreated = await Temperament.create({
+      name,
+    });
 
     res
       .status(201)
@@ -80,6 +85,7 @@ const createTemperament = async (req, res, next) => {
 const getAllTemperaments = async (req, res, next) => {
   try {
     const allTemperament = await Temperament.findAll();
+
     return res.status(200).json(allTemperament);
   } catch (error) {
     return res
