cec9d122f716dd9ac2487382fc2497d9008bc34f
Jorge L. Mejia Pejerrey <mejiapj@gmail.com>
2023-05-25 00:01:28 -0500

    Mejoras al db

--- api/src/db.js
+++ api/src/db.js
@@ -6,6 +6,7 @@ const { DB_USER, DB_PASSWORD, DB_HOST } = process.env;
 
 const sequelize = new Sequelize(
   `postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}/dogs`,
+  // DB_PLOY,
   {
     logging: false, // set to console.log to see the raw SQL queries
     native: false, // lets Sequelize know we can use pg-native for ~30% more speed
@@ -49,6 +50,7 @@ Dog.belongsToMany(Temperament, {
 Temperament.belongsToMany(Dog, {
   through: 'DogTemperament',
   timestamps: false,
+  onDelete: 'RESTRICT',
 });
 
 module.exports = {
